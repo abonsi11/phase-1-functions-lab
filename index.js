6256660b16371f2ac89450035e7ea71437157fa0
Anthony Bonsi <xbonsi96@gmail.com>
2021-10-23 11:41:00 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const Hq = 42
 
-function distanceFromHqInBlocks(){
-    console.log(43-42)
+function distanceFromHqInBlocks(streetNumber){
+    console.log(streetNumber - Hq)
+    let distance = streetNumber - Hq
+    if(distance < 0){
+        distance = distance * -1
+    }
+    return distance
+}
+function distanceFromHqInFeet(streetNumber){
+    console.log(streetNumber - Hq)
+    let distance = streetNumber - Hq  
+    if(distance < 0){
+        distance = distance * -1
+    }
+    return distance * 264
+}
+function distanceTravelledInFeet(streetNumber){
+    console.log(streetNumber1 - streetnumber2)
+    let distance = streetNumber1 - streetNumber2
+    if(distance < 0){
+        distance = distance * -1
+    }
+    return distance * 264
 }
