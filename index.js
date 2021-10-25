a478e90b8b65b3b3145f6bbd250a2c7a49835de3
Anthony Bonsi <xbonsi96@gmail.com>
2021-10-25 15:08:52 -0600

    Done.

--- index.js
+++ index.js
@@ -25,9 +25,18 @@ function distanceTravelledInFeet(streetNumber1, streetNumber2){
     return distance * 264
 }
 function calculatesFarePrice(streetNumber1, streetNumber2){
-    console.log(streetNumber1 - streetNumber2)
-        let distance = streetNumber1 - streetNumber2
-        if(distance < 0){
-            distance = distance * -1
-        }
+    let feet = distanceTravelledInFeet(streetNumber1, streetNumber2)
+    let sample = 400
+    if(feet < 400){
+        return 0
+    }
+    else if(feet >= 400 && feet < 2000){
+        return (feet - sample) * 0.02
+    }
+    else if(feet >= 2000 && feet < 2500){
+    return 25
+    }
+    else {
+        return 'cannot travel that far'
+    }
 }
